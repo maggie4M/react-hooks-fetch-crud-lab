bc9456f9bb34737740f1286936c8c2552bc837a7
Margaret <mungaimargaretn@gmail.com>
2024-05-09 14:46:51 +0300

    Finished Assignment

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,12 +1,50 @@
 import React from "react";
 
-function QuestionList() {
+function QuestionItem({ question, onDeleteQuestions, onUpdateAnswer }) {
+  const { id, prompt, answers, correctIndex } = question;
+ 
+  const options = answers.map((answer, index) => (
+    <option key={index} value={index}>
+      {answer}
+    </option>
+     
+  ));
+
+  function handleDeleteClick() {
+      fetch(`http://localhost:4000/questions/${id}`, {
+        method: "DELETE"
+      })
+      .then((resp) => resp.json())
+      .then(() => onDeleteQuestions(question))
+  }
+
+  function handleUpdateOptions(event) {
+    const index = parseInt(event.target.value, 10)
+
+    fetch(`http://localhost:4000/questions/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          "correctIndex": {index}
+        })
+    })
+    .then((resp) => resp.json())
+    .then((updateAnswer) => onUpdateAnswer(updateAnswer))
+  }
+
   return (
-    <section>
-      <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
-    </section>
+    <li>
+      <h4>Question {id}</h4>
+      <h5>Prompt: {prompt}</h5>
+      <label>
+        Correct Answer:
+        <select defaultValue={correctIndex} onChange={handleUpdateOptions}>{options}</select>
+      </label>
+      <button onClick={handleDeleteClick}>Delete Question</button>
+    </li>
   );
 }
 
-export default QuestionList;
+export default QuestionItem;
